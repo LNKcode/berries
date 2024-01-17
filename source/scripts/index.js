1b2dd7cba2588d58b419d985f4aa85bffca14b82
Igor <3-tv-n@mail.ru>
2024-01-18 02:05:03 +0300

    Сделал INDEX

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
+const btn = document.querySelector('.header__burger');
+
+header.classList.remove('header--no-js');
+header.classList.add('header--close');
+btn.addEventListener('click', ()=> {
+  if (header.classList.contains('header--close')) {
+    header.classList.remove('header--close');
+    header.classList.add('header--open');
+  } else {
+    header.classList.add('header--close');
+    header.classList.remove('header--open');
+  }
+});
